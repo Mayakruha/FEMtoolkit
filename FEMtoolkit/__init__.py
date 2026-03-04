ce9f075b6b03b35367cc09c7a0894ff5b855f51c
Mayakruha <49606586+Mayakruha@users.noreply.github.com>
2026-03-04 19:37:59 +0100

    Update __init__.py

--- FEMtoolkit/__init__.py
+++ FEMtoolkit/__init__.py
@@ -4,7 +4,12 @@ SymmetryEquations_abq,
 EstFaces,
 Normals,
 import_ndload,
+import_fcload,
 export_ndload,
+export_fcload,
 CreateSubmodel,
-Make3DLinearMesh)
+Make3DLinearMesh,
+MeshFromFaceLoad,
+mapping,
+map_surf)
 from . import morphing
